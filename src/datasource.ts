83eedf05ebda2830755b92f0c61495bd7c065065
Ian <ian.w.lloyd@me.com>
2024-04-28 09:47:28 +0800

    Update schema and refactor code for dynamic table reference
    The schema has been updated to support new field types and to allow parameters to be dynamic. The code has been refactored accordingly to adapt to the new schema, particularly to permit selection of specific tables, rather than referring to a singular, static one. As part of this change, "document" has been replaced with a more generic term "json", providing more flexibility and less ambiguity.

--- src/datasource.ts
+++ src/datasource.ts
@@ -5,118 +5,149 @@ import { QldbDriver } from "amazon-qldb-driver-nodejs";
 import { asJson } from "./qldb/helpers";
 import * as ion from "ion-js";
 
+type Json = Record<string, unknown>;
+type Extra = { table: string } & Record<string, string>;
+
+type Query = { extra: Extra };
+type WithJson = { json: Json } & Query;
+type WithId = { id: string } & Query;
+type WithWhere = { where: Json } & Query;
+type WithField = { field: string } & Query;
+
 class AmazonQldbDatasource implements IntegrationBase {
   private readonly region: string;
   private readonly ledger: string;
-  private readonly table: string;
-  private readonly repository: QldbRepository;
+  private readonly repositoryOptions;
+  private readonly repositories: WeakMap<Symbol, QldbRepository> =
+    new WeakMap();
 
-  constructor(config: { region: string; ledger: string; table: string }) {
+  constructor(config: { region: string; ledger: string; idFieldName: string }) {
     this.region = config.region;
     this.ledger = config.ledger;
-    this.table = config.table;
-    this.repository = new QldbRepository(this.table, {
-      idFieldName: "documentId",
-    });
+
+    // Map to hold repositories; we'll use a WeakMap to allow it to purge old repositories when not in use
+    this.repositoryOptions = {
+      idFieldName: config.idFieldName,
+    };
   }
 
   protected get db(): QldbDriver {
     return QldbFactory.get(this.ledger, this.region);
   }
 
-  async create(query: { json: Record<string, unknown> }) {
-    return this.db.executeLambda(async (txn) =>
-      this.normalise(await this.repository.insert(txn, query.json)),
-    );
-  }
-
-  async read(query: { json: Record<string, unknown> }) {
-    return this.db.executeLambda(async (txn) =>
-      this.normalise(await this.repository.where(txn, query.json)),
-    );
-  }
-
-  async readById(query: { id: string }) {
-    return this.db.executeLambda(async (txn) =>
-      this.normalise(await this.repository.find(txn, query.id)),
-    );
-  }
-
-  async update(query: {
-    document: Record<string, unknown>;
-    where: Record<string, unknown>;
-  }) {
+  async create(query: WithJson) {
     return this.db.executeLambda(async (txn) =>
       this.normalise(
-        await this.repository.update(txn, query.document, query.where),
+        await this.repository(query.extra.table).insert(txn, query.json),
       ),
     );
   }
 
-  async updateById(query: { document: Record<string, unknown>; id: string }) {
+  async read(query: WithJson) {
     return this.db.executeLambda(async (txn) =>
       this.normalise(
-        await this.repository.update(txn, query.document, {
-          documentId: query.id,
-        }),
+        await this.repository(query.extra.table).where(txn, query.json),
       ),
     );
   }
 
-  async upsert(query: {
-    document: Record<string, unknown>;
-    where: Record<string, unknown>;
-  }) {
+  async readById(query: WithId) {
     return this.db.executeLambda(async (txn) =>
       this.normalise(
-        await this.repository.upsert(txn, query.document, query.where),
+        await this.repository(query.extra.table).find(txn, query.id),
       ),
     );
   }
 
-  async insertInto(query: {
-    field: string;
-    document: Record<string, unknown>;
-    where: Record<string, unknown>;
-  }) {
+  async update(query: WithJson & WithWhere) {
     return this.db.executeLambda(async (txn) =>
       this.normalise(
-        await this.repository.insertInto(
+        await this.repository(query.extra.table).update(
           txn,
-          query.field,
-          query.document,
+          query.json,
           query.where,
         ),
       ),
     );
   }
 
-  async insertIntoById(query: {
-    field: string;
-    document: Record<string, unknown>;
-    id: string;
-  }) {
+  async updateById(query: WithId & WithWhere & WithJson) {
     return this.db.executeLambda(async (txn) =>
       this.normalise(
-        await this.repository.insertInto(txn, query.field, query.document, {
+        await this.repository(query.extra.table).update(txn, query.json, {
           documentId: query.id,
         }),
       ),
     );
   }
 
-  async delete(query: { json: Record<string, unknown> }) {
+  async upsert(query: WithJson & WithWhere) {
     return this.db.executeLambda(async (txn) =>
-      this.normalise(await this.repository.delete(txn, query.json)),
+      this.normalise(
+        await this.repository(query.extra.table).upsert(
+          txn,
+          query.json,
+          query.where,
+        ),
+      ),
     );
   }
 
-  async deleteById(query: { id: string }) {
+  async insertInto(query: WithJson & WithWhere & WithField) {
     return this.db.executeLambda(async (txn) =>
       this.normalise(
-        await this.repository.delete(txn, { documentId: query.id }),
+        await this.repository(query.extra.table).insertInto(
+          txn,
+          query.field,
+          query.json,
+          query.where,
+        ),
       ),
     );
+  }
+
+  async insertIntoById(query: WithJson & WithId & WithField) {
+    return this.db.executeLambda(async (txn) =>
+      this.normalise(
+        await this.repository(query.extra.table).insertInto(
+          txn,
+          query.field,
+          query.json,
+          {
+            documentId: query.id,
+          },
+        ),
+      ),
+    );
+  }
+
+  async delete(query: WithJson) {
+    return this.db.executeLambda(async (txn) =>
+      this.normalise(
+        await this.repository(query.extra.table).delete(txn, query.json),
+      ),
+    );
+  }
+
+  async deleteById(query: WithId) {
+    return this.db.executeLambda(async (txn) =>
+      this.normalise(
+        await this.repository(query.extra.table).delete(txn, {
+          documentId: query.id,
+        }),
+      ),
+    );
+  }
+
+  protected repository(table: string): QldbRepository {
+    const key = Symbol.for(table);
+    if (!this.repositories.has(key))
+      this.repositories.set(key, this.buildRepository(table));
+    return this.repositories.get(key)!;
+  }
+
+  protected buildRepository(table: string): QldbRepository {
+    return new QldbRepository(table, this.repositoryOptions);
   }
 
   protected normalise(value: ion.dom.Value): object;
